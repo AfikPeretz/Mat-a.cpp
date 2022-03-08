e0febfa5aa4352b5b6b1df516f9629a21becd1c3
Afik Peretz <73401652+AfikPeretz@users.noreply.github.com>
2022-03-08 17:38:39 +0200

    Add files via upload

---
+++ mat-a-main/mat.cpp
@@ -0,0 +1,15 @@
+#include "mat.hpp"
+#include <iostream>
+#include <string>
+using namespace std;
+using namespace ariel;
+
+
+namespace ariel
+{
+    string mat (int row, int column, char symbol1, char symbol2){
+        string a = "aa";
+        return a;
+    }
+    
+}
---
+++ mat-a-main/mat.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+
+namespace ariel {
+    string mat (int row, int column, char symbol1, char symbol2);
+}
